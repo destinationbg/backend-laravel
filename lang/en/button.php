90e735ee4a85f2a5529418f0c0c67bdb179b9bce
XenBG <wildbeastbg@gmail.com>
2023-10-08 00:45:08 +0300

    Added `villages` views

--- lang/en/button.php
+++ lang/en/button.php
@@ -13,5 +13,6 @@ return [
     'nsi_cities' => 'View all the cities on the NRNM website',
     'nsi_municipalities' => 'View all the municipalities on the NRNM website',
     'nsi_provinces' => 'View all the provinces on the NRNM website',
+    'nsi_villages' => 'View all the villages on the NRNM website',
     'register' => 'Register',
 ];
